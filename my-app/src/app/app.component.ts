db1c09d7078edfdc3fe1f063449470f23c14d735
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-05-27 17:43:00 +0530

    written html and css first time in angular in the created component

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: ` <div>Hola</div> `,
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+    <div>Hola</div>
+    <app-home></app-home>
+  `,
+
   styleUrl: './app.component.css',
 })
 export class AppComponent {
